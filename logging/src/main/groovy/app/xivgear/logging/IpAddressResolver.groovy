a7933aa33c73b8991d3ef5ab00a1c0c71526e4f3
XP <smurfs@smurfs.verymad.net>
2025-06-17 18:15:43 -0700

    Fix logging

--- logging/src/main/groovy/app/xivgear/logging/IpAddressResolver.groovy
+++ logging/src/main/groovy/app/xivgear/logging/IpAddressResolver.groovy
@@ -1,12 +1,18 @@
 package app.xivgear.logging
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import io.micronaut.context.annotation.Context
 import io.micronaut.http.HttpRequest
 import jakarta.inject.Singleton
 
-@Singleton
 @CompileStatic
+@Slf4j
+@Context
 class IpAddressResolver {
+	public IpAddressResolver() {
+		log.info("Foo")
+	}
 
 	String resolveIp(HttpRequest<?> request) {
 		return request.headers
